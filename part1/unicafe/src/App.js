0331a4c352add543df9070636f6e7d6c347a36cc
russell-loube-optimizely <russell.loube@optimizely.com>
2023-04-15 09:27:20 -0500

    Refactored statistics component, WIP conditional rendering

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,70 +1,143 @@
 import { useState } from "react";
 
-const Button = ({ handleClick, text }) => {
-  return <button onClick={handleClick}>{text}</button>;
+const Button = ({ handleClick, text1 }) => {
+  return <button onClick={handleClick}>{text1}</button>;
 };
 
-const Header = ({ text }) => {
-  return <h1>{text}</h1>;
+const Header = ({ text1 }) => {
+  return <h1>{text1}</h1>;
 };
 
-const Counter = ({ text, count }) => {
+const Counter = ({ text1, count, text2 }) => {
   return (
     <div>
-      {text} {count}
+      {text1} {count} {text2}
     </div>
   );
 };
 
+const Statistics = (props) => {
+  console.log(props);
+  console.log(`all: ${props.categories[4].count}`);
+  if (props.categories[4].count === 0) {
+    return <div>No feedback given</div>;
+  }
+
+  return (
+    <>
+      <Header text1={props.header} />
+      <Counter
+        text1={props.categories[0].name}
+        count={props.categories[0].count}
+      />
+      <Counter
+        text1={props.categories[1].name}
+        count={props.categories[1].count}
+      />
+      <Counter
+        text1={props.categories[2].name}
+        count={props.categories[2].count}
+      />
+      <Counter
+        text1={props.categories[3].name}
+        count={props.categories[3].count}
+      />
+      <Counter
+        text1={props.categories[4].name}
+        count={props.categories[4].count}
+      />
+      <Counter
+        text1={props.categories[5].name}
+        count={props.categories[5].count}
+        text2="%"
+      />
+    </>
+  );
+};
+
 const App = () => {
-  console.log("rendered");
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [sum, setSum] = useState(0);
   const [all, setAll] = useState(0);
-  const [total, setTotal] = useState(0);
   const [average, setAverage] = useState(0);
+  const [positive, setPositive] = useState(0);
 
-  const handleGlobalClick = () => {
+  const handleGlobalClick = (buttonValue, totalGood) => {
     const updatedAll = all + 1;
     setAll(updatedAll);
-    const updatedTotal = total + 1;
-    setTotal(updatedTotal);
-    const updatedAverage = total / all;
+    console.log(`updatedAll: ${updatedAll}`);
+    const updatedSum = sum + buttonValue;
+    setSum(updatedSum);
+    console.log(`updatedSum: ${updatedSum}`);
+    const updatedAverage = updatedSum / updatedAll;
     setAverage(updatedAverage);
+    console.log(`updatedAverage: ${updatedAverage}`);
+    const updatedPositive = (totalGood * 100) / updatedAll;
+    setPositive(updatedPositive);
   };
 
   const handleGoodClick = () => {
     const updatedGood = good + 1;
     setGood(updatedGood);
-    handleGlobalClick();
+    console.log(`updatedGood: ${updatedGood}`);
+    handleGlobalClick(1, updatedGood);
   };
 
   const handleNeutralClick = () => {
     const updatedNeutral = neutral + 1;
     setNeutral(updatedNeutral);
-    handleGlobalClick();
+    console.log(`updatedNeutral: ${updatedNeutral}`);
+    handleGlobalClick(0, good);
   };
 
   const handleBadClick = () => {
     const updatedBad = bad + 1;
     setBad(updatedBad);
-    handleGlobalClick();
+    console.log(`updatedBad: ${updatedBad}`);
+    handleGlobalClick(-1, good);
+  };
+
+  const statistics = {
+    header: "statistics",
+    categories: [
+      {
+        name: "good",
+        count: good,
+      },
+      {
+        name: "neutral",
+        count: neutral,
+      },
+      {
+        name: "bad",
+        count: bad,
+      },
+      {
+        name: "all",
+        count: all,
+      },
+      {
+        name: "average",
+        count: average,
+      },
+      {
+        name: "positive",
+        count: positive,
+      },
+    ],
   };
 
   return (
     <div>
-      <Header text="give feedback" />
-      <Button handleClick={handleGoodClick} text="good" />
-      <Button handleClick={handleNeutralClick} text="neutral" />
-      <Button handleClick={handleBadClick} text="bad" />
-      <Header text="statistics" />
-      <Counter text="good" count={good} />
-      <Counter text="neutral" count={neutral} />
-      <Counter text="bad" count={bad} />
-      <Counter text="all" count={all} />
-      <Counter text="average" count={average} />
+      <Header text1="give feedback" />
+      <Button handleClick={handleGoodClick} text1="good" />
+      <Button handleClick={handleNeutralClick} text1="neutral" />
+      <Button handleClick={handleBadClick} text1="bad" />
+      <Header text1={statistics.header} />
+      <Statistics categories={statistics.categories} />
     </div>
   );
 };

92734f91841fd3b72082b7ed60f81e47381803a6
rjloube <rjloube@gmail.com>
2023-09-22 08:34:01 -0500

    rendering single country wip, bug with initial country results

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,6 +1,30 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
+const SingleCountry = (props) => {
+  if (props.displayedCountries.length !== 1) {
+    return
+  } else {
+    return (
+      <p>single country here</p>
+    )
+  }
+  // <>
+  //   <h1>{props.commonName}</h1>
+  //   <p>
+  //     capital {props.capital}
+  //     area {props.area}
+  //   </p>
+  //   <h2>languages:</h2>
+  //   <ul>
+  //     {props.languages.map((language) => (
+  //       <li>language</li>
+  //     ))}
+  //   </ul>
+  //   <img>{props.flagImage}</img>
+  // </>;
+};
+
 const App = () => {
   const [value, setValue] = useState("");
   const [country, setCountry] = useState(null);
@@ -28,23 +52,36 @@ const App = () => {
         .includes(event.target.value.toLowerCase());
     });
     console.log("newDisplayedCountries:", newDisplayedCountries);
-    if (newDisplayedCountries.length > 10) {
+    if (newDisplayedCountries.length === commonNames.length) {
+      setDisplayedCountries();
+    } else if (newDisplayedCountries.length > 10) {
       setDisplayedCountries("Too many matches, specify another filter");
+    } else if (newDisplayedCountries.length === 1) {
+      setDisplayedCountries(newDisplayedCountries);
+      axios
+        .get(
+          `https://studies.cs.helsinki.fi/restcountries/api/name/${newDisplayedCountries[0]}`
+        )
+        .then((response) => {
+          console.log("Single country data:", response.data);
+        });
     } else {
-      const formattedDisplayedCountries = newDisplayedCountries.map(newDisplayedCountry => newDisplayedCountry + '\n');
-      // See https://stackoverflow.com/questions/61768544/n-is-not-rendering-the-text-in-new-line as to why this isn't working in HTML
-      console.log('formattedDisplayedCountries:', formattedDisplayedCountries);
+      const formattedDisplayedCountries = newDisplayedCountries.map(
+        (newDisplayedCountry) => newDisplayedCountry + "\n"
+      );
+      console.log("formattedDisplayedCountries:", formattedDisplayedCountries);
       setDisplayedCountries(formattedDisplayedCountries);
     }
   };
 
   return (
-    <div>
+    <div style={{ whiteSpace: "pre-line" }}>
       <form>
         find countries
         <input onChange={handleCountryChange} />
       </form>
       <p>{displayedCountries}</p>
+      {/* <SingleCountry displayedCountries={displayedCountries} /> */}
     </div>
   );
 };
